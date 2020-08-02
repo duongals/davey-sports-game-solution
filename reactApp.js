50093ea4b4c60457aa9fc4eef2c933bd77a5e991
tamburkey <petem14@gmail.com>
2020-08-02 14:57:11 -0400

    update image paths

--- reactApp.js
+++ reactApp.js
@@ -146,22 +146,22 @@ class Game extends React.Component {
 function App(props) {
   const raccoons = {
     name: 'Russiaville Raccoons',
-    logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/raccoon.png'
+    logoSrc: './assets/images/raccoon.png'
   }
 
   const squirrels = {
     name: 'Sheridan Squirrels',
-    logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/squirrel.png'
+    logoSrc: './assets/images/squirrel.png'
   }
 
   const bunnies = {
     name: 'Burlington Bunnies',
-    logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/bunny.png'
+    logoSrc: './assets/images/bunny.png'
   }
 
   const hounds = {
     name: 'Hammond Hounds',
-    logoSrc: 'https://raw.githubusercontent.com/p-mayor/davey-react-sports-lab-basic/master/assets/images/hound.png'
+    logoSrc: './assets/images/hound.png'
   }
 
   return (
